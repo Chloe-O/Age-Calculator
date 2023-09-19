258b87188dafffc396ff5560bf1b3b5558e530ed
Chloe-O <chloeosbourne@yahoo.co.uk>
2023-09-19 21:19:50 +0100

    starting js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+let yearResult = $('#year-result p span');
+let currentYear = new Date().getFullYear();
 
-if ( $('#day:input') < 1 || $('#day:input') > 31 ) {
-    console.log('thats not valid');
-};
+console.log( currentYear );
 
+$('.submit').on('click', () => {
+    let yearsAlive = currentYear - $('#year').val();
+    console.log(yearsAlive);
+});
+
+console.log(currentYear - $('#year').val())
+
+// $('#year-result p span').text(29)
 
