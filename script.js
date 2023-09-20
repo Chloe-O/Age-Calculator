116d34774b0a275d65335a1b28d6db0cd4a89e09
Chloe-O <chloeosbourne@yahoo.co.uk>
2023-09-20 19:27:11 +0100

    year in DOM

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-let yearResult = $('#year-result p span');
-let currentYear = new Date().getFullYear();
+let getDate = new Date();
+let currentYear = getDate.getFullYear();
+let currentMonth = getDate.getMonth();
+let currentDay = getDate.getDay();
 
-console.log( currentYear );
+let yearResult = $("#year-result p span");
+let monthResult = $("#month-result p span");
+let dayResult = $("#day-result p span");
 
-$('.submit').on('click', () => {
-    let yearsAlive = currentYear - $('#year').val();
-    console.log(yearsAlive);
+let counter = 0;
+
+$(".submit").on("click", (e) => {
+  let yearsAlive = currentYear - parseInt($("#year").val());
+
+  // for (let counter = 0; counter <= yearsAlive; counter++ ) {
+  //   yearResult.text(counter);
+  // };
+
+  e.preventDefault();
 });
 
-console.log(currentYear - $('#year').val())
+// function calculate() {
 
-// $('#year-result p span').text(29)
-
+// }
